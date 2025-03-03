39018d54f21008d8edb5c34f570e082916d44152
shotakami7@gmail.com <shotakami7@gmail.com>
2025-03-03 02:31:37 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -8,3 +8,35 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def drop_down_1_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def drop_down_2_change(self, **event_args):
+    """This method is called when an item is selected"""
+    pass
+
+  def text_box_1_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def text_area_1_change(self, **event_args):
+    """This method is called when the text in this text area is edited"""
+    pass
+
+  def button_1_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    pass
+
+  def text_box_2_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    pass
+
+  def link_1_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_2_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
