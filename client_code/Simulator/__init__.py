b1ec49b4e7dc7d6b68bec1a99b22d9c3a70d7d9e
shotakami7@gmail.com <shotakami7@gmail.com>
2025-03-03 05:09:00 +0000

    Created Simulator, References and HowToUse; Edited settings; Deleted Form1

--- client_code/Simulator/__init__.py
+++ client_code/Simulator/__init__.py
@@ -1,8 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import SimulatorTemplate
 from anvil import *
+import anvil.server
 
 
-class Form1(Form1Template):
+class Simulator(SimulatorTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
