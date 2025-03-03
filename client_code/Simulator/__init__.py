6a30e67ab1a3e66977875d2f6887b50c1bef236c
shotakami7@gmail.com <shotakami7@gmail.com>
2025-03-03 07:25:30 +0000

    Edited Simulator

--- client_code/Simulator/__init__.py
+++ client_code/Simulator/__init__.py
@@ -2,42 +2,52 @@ from ._anvil_designer import SimulatorTemplate
 from anvil import *
 import anvil.server
 
-
 class Simulator(SimulatorTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-
-  def pump_change(self, **event_args):
-    """This method is called when an item is selected"""
-    pass
-
-  def tank_change(self, **event_args):
-    """This method is called when an item is selected"""
-    pass
-
-  def rho_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
-
-  def text_area_1_change(self, **event_args):
-    """This method is called when the text in this text area is edited"""
-    pass
-
-  def button_1_click(self, **event_args):
+  
+  def calculate_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    Q_solution, t_fill = anvil.server.call('anvilSolver',
+                                           rho=float(self.rho.text),
+                                           L=float(self.L.text),
+                                           D=float(self.D.text)/12,
+                                           h_elevation=float(self.h_elevation.text),
+                                           f=float(self.f.text),
+                                           K_minor=float(self.K_minor.text),
+                                           LossVar=float(self.LossVar.text),
+                                           selected_pump=self.pump.selected_value,
+                                           selected_tank=self.tank.selected_value)
 
-  def L_pressed_enter(self, **event_args):
-    """This method is called when the user presses Enter in this text box"""
-    pass
+    if Q_solution:
+      self.flow_rate_result.visible = True
+      self.flow_rate_result.text = "Calculated Flow Rate = " + str(Q_solution) + " gpm"
 
-  def link_1_click(self, **event_args):
+    if t_fill:
+      self.fill_time_result.visible = True
+      self.fill_time_result.text = "Calculated Fill Time = " + str(t_fill) + " min"
+
+    
+
+  def how_to_use_click(self, **event_args):
     """This method is called when the link is clicked"""
     pass
 
-  def link_2_click(self, **event_args):
+  def simulator_click(self, **event_args):
     """This method is called when the link is clicked"""
+    pass
+
+  def references_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def form_refreshing_data_bindings(self, **event_args):
+    """This method is called when refresh_data_bindings is called"""
+    pass
+
+  def K_minor_change(self, **event_args):
+    """This method is called when the text in this text box is edited"""
     pass
