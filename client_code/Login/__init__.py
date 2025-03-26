e9ac30d66e2a95cf92662e7410c9bdc930a20deb
shotakami7@gmail.com <shotakami7@gmail.com>
2025-03-26 16:44:17 +0000

    Created Login.Simulator; Edited Login; Deleted Simulator

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -5,11 +5,21 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ..Simulator import Simulator
 
 
 class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
+
     # Any code you write here will run before the form opens.
+
+  def login_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    if (self.username.text != 'test_user') or (self.password.text != '12345'):
+      alert('Username or password is not correct.')
+    else:
+      self.visible = False
+      self.
       
