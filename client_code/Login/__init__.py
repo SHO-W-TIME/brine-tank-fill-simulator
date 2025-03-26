6542ab165141b9cf2e7e05c2c5ed2dc2ca8a9d46
shotakami7@gmail.com <shotakami7@gmail.com>
2025-03-26 16:12:13 +0000

    Edited Login

--- client_code/Login/__init__.py
+++ client_code/Login/__init__.py
@@ -11,12 +11,5 @@ class Login(LoginTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
     # Any code you write here will run before the form opens.
-
-  def login_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    if (self.username.text != 'test_user') or (self.password.text != 'insertpasswordhere'):
-      alert('User name or password is incorrect.')
-    else
       
