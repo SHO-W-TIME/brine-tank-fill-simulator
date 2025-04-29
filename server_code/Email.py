4f7b5cc54aab9afef632f553340e2043e150d9a7
gravekeepermain@gmail.com <gravekeepermain@gmail.com>
2025-04-29 16:44:10 +0000

    Created Email; Deleted ServerModule1

---
+++ server_code/Email.py
@@ -0,0 +1,15 @@
+import anvil.files
+from anvil.files import data_files
+import anvil.email
+import anvil.users
+import anvil.server
+
+#Email service
+@anvil.server.callable
+def send_feedback(name, email, feedback):
+    anvil.email.send(to="janetphan.work@gmail.com", #change this email once we're done!!
+                     subject=f"Feedback from {name},",
+                     text=f"""
+                     {name} has sent feedback from the brine tank simulator. To reply, send a message to their email {email}. Their message is as follows:
+                     {feedback}
+                     """)
